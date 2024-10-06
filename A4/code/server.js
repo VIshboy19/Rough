7d5ac484702e8c5fdad911aa6cec91b1b0d3ba20
VIshboy19 <152842659+VIshboy19@users.noreply.github.com>
2024-10-06 13:39:13 -0700

    Update server.js

--- A4/code/server.js
+++ A4/code/server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || '50.16.118.249';
+var hostname = process.env.HOSTNAME || '34.204.88.233';
 var port = 8080;
 
 app.get("/", function (req, res) {
