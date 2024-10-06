41d8df6a2cd5f71fc16707d164144f5db9d59b0f
VISHRUT <vishrutchawla@gmail.com>
2024-10-06 13:45:57 -0700

    Latest IP

--- A4/code/server.js
+++ A4/code/server.js
@@ -3,16 +3,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || '50.16.118.249';
+var hostname = process.env.HOSTNAME || '34.204.88.233';
 var port = 8080;
 
-app.get("/", function (req, res) {
-    res.redirect("index.html")
-});
+// Automatically serve index.html from the public directory
+app.use(express.static(__dirname + '/public'));
 
 app.use(methodOverride());
-app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
